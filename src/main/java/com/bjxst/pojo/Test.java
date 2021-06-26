4cb51cd3351f9d720fa85091049799dd5cd3153d
XXXIIUIIXXX <1181130270@qq.com>
2021-06-26 10:25:42 +0800

    hot-fix push

---
+++ src/main/java/com/bjxst/pojo/Test.java
@@ -0,0 +1,15 @@
+package com.bjxst.pojo;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        i = i++;
+        int j = i++;
+        int k = i+ ++i*i++;
+        int n = i+ ++i*i++ *(++i);
+        System.out.println("i="+i);
+        System.out.println("j="+j);
+        System.out.println("k="+k);
+        System.out.println("n="+n);
+    }
+}
