ade25e98ec1237d0073a8fbbf28b6110ba4c9cf6
XXXIIUIIXXX <58926309+XXXIIUIIXXX@users.noreply.github.com>
2021-06-27 17:27:24 +0800

    ajdajfas

--- src/main/java/com/bjxst/pojo/GitTest.java
+++ src/main/java/com/bjxst/pojo/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("pull git");
+        System.out.println("pull git");
+        System.out.println("pull git");
     }
 }
